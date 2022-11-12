bb30451234ed381a2e9503a448bef221c40d3439
William Desportes <williamdes@wdes.fr>
2022-11-12 16:35:09 +0100

    Remove the need of an action for creating a release [ci skip]

---
+++ create-github-release.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+##
+# @license http://unlicense.org/UNLICENSE The UNLICENSE
+# @author William Desportes <williamdes@wdes.fr>
+##
+
+set -eu
+
+function generate_post_data {
+  cat <<EOF
+{
+  "tag_name": "${TAG_NAME}",
+  "owner": "${REPO_OWNER}",
+  "repo": "${REPO_REPO}",
+  "body": "${BODY}",
+  "draft": true,
+  "make_latest": ${MAKE_LATEST},
+  "prerelease": ${PRE_RELEASE}
+}
+EOF
+}
+curl \
+    -H "Authorization: token ${GITHUB_TOKEN}" \
+    -H "Accept: application/vnd.github+json" \
+    --data "$(generate_post_data)" "https://api.github.com/repos/$user/$repo/releases"
