782bf7edd762c3be62d67115095d34118652f38c
William Desportes <williamdes@wdes.fr>
2022-11-12 16:50:45 +0100

    Fix API calls
    [ci skip]

--- create-github-release.sh
+++ create-github-release.sh
@@ -10,8 +10,6 @@ function generate_post_data {
   cat <<EOF
 {
   "tag_name": "${TAG_NAME}",
-  "owner": "${REPO_OWNER}",
-  "repo": "${REPO_NAME}",
   "body": "${BODY}",
   "draft": true,
   "make_latest": ${MAKE_LATEST},
@@ -19,7 +17,8 @@ function generate_post_data {
 }
 EOF
 }
+
 curl \
     -H "Authorization: token ${GITHUB_TOKEN}" \
     -H "Accept: application/vnd.github+json" \
-    --data "$(generate_post_data)" "https://api.github.com/repos/$user/$repo/releases"
+    --data "$(generate_post_data)" "https://api.github.com/repos/${REPO_SLUG}/releases"
